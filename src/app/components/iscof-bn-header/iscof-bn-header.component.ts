91978746da3e6087256200194a0219b4e769c5a1
John <johnmarksalapantan@gmail.com>
2022-10-07 22:46:31 +0800

    tut number 6 YAY next tut 7

---
+++ src/app/components/iscof-bn-header/iscof-bn-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iscof-bn-header',
+  templateUrl: './iscof-bn-header.component.html',
+  styleUrls: ['./iscof-bn-header.component.scss'],
+})
+export class IscofBnHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+import { HomePage } from './home.page';
+import { IscofBnHeaderComponent } from 'src/app/components/iscof-bn-header/iscof-bn-header.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [
+    HomePage,
+    IscofBnHeaderComponent
+  ]
+})
+export class HomePageModule {}
